9970c57edd5b42eca41c69fd12a4444a2513b049
Sosou222 <szym.baran222@gmail.com>
2025-09-26 23:26:47 +0200

    updated target point static

--- Assets/Project/Scripts/Enemy/Targeting/TargetPointStatic.cs
+++ Assets/Project/Scripts/Enemy/Targeting/TargetPointStatic.cs
@@ -4,7 +4,12 @@ public class TargetPointStatic : ITargetPoint
 {
     private Vector3 point;
 
-    public TargetPointStatic(Vector3 point)
+    public TargetPointStatic()
+    {
+        point = PlayerInfoSystem.Instance.GetPosition();
+    }
+
+    public void SetPoint(Vector3 point)
     { 
         this.point = point; 
     }
