92d76bc93b2a179750444e3477b9df4dae86c944
Sosou222 <szym.baran222@gmail.com>
2025-11-01 23:28:28 +0100

    disabled enemy view on its death

--- Assets/Project/Scripts/Enemy/Enemy.cs
+++ Assets/Project/Scripts/Enemy/Enemy.cs
@@ -43,6 +43,7 @@ public class Enemy : MonoBehaviour
     {
         animator.TriggerDeath();
         DropsSystem.Instance.Spawn(transform.position);
+        EnemyView.gameObject.SetActive(false);
         animator.DeathAnimationFinished += () => Destroy(gameObject);
     }
 
